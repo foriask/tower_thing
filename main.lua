d7b422252fae049074b8b25ae488c72464d1dcab
foriask <Foriask16@gmail.com>
2026-05-23 23:47:41 +0200

    Nothing really...

--- main.lua
+++ main.lua
@@ -6,37 +6,22 @@ Fps = 0
 Bullet_count = 0
 how_many_bullets = 1
 function love.load()
+	local img = love.graphics.newImage("sylv.png")
+
+	psystem = love.graphics.newParticleSystem(img, 32)
+	psystem:setParticleLifetime(2, 5) -- Particles live at least 2s and at most 5s.
+	psystem:setEmissionRate(5)
+	psystem:setSizeVariation(1)
+	psystem:setLinearAcceleration(-20, -20, 20, 20) -- Random movement in all directions.
+	psystem:setColors(1, 1, 1, 1, 1, 1, 1, 0) -- Fade to transparency.
 	tree_manager.item_groups.enemies[1] = {
 		position = { 2, 3 },
 		s = { 50, 50 },
-		collider = { type = "c", [1] = 2, [2] = 3, [3] = 52, [4] = 53, radius = 100 },
+		collider = { type = "c", [1] = 2, [2] = 3, [3] = 52, [4] = 53, radius = 20 },
 		damage = function(_dmg)
 			print(_dmg)
 		end,
 	}
-
-	local tabla_prueba = { patata = 20 }
-	function print_table(_table)
-		for _i, _arg in pairs(_table) do
-			print(type(_arg))
-			if type(_arg) == "table" then
-				print(" ")
-			else
-				if type(_arg) == "function" then
-					print(" ")
-				else
-					if type(_arg) == "boolean" then
-						print(" ")
-					else
-						print(_i .. " " .. _arg .. " ")
-					end
-				end
-			end
-		end
-	end
-	print_table(tabla_prueba)
-
-	-- enemies_manager:set_enemie(false, { 300, 300, 0 }, { 30, 30 }, 2, { type = "c", radius = 30 })
 end
 
 function love.update(dt)
@@ -49,19 +34,21 @@ function love.update(dt)
 	how_many_bullets = how_many_bullets
 		+ (love.keyboard.isDown("t") and 1 or 0)
 		+ (love.keyboard.isDown("y") and -1 or 0)
+
 	if love.keyboard.isDown("e") then
 		local _bullets = how_many_bullets
 		while _bullets > 0 do
 			local _bullet = bullet_manager:new(nil, { 400 * math.random(), 1600 * math.random() }, { 5, 0.5 }, 30)
-			print_table(_bullet)
+			-- print_table(_bullet)
 			table.insert(tree_manager.item_groups.bullets, _bullet)
 			_bullets = _bullets - 1
 		end
 	end
 
 	-- A few functions that make the game work. I don't know why dont ask. Remember to use DT!
-	tree_manager.item_groups.bullets =
-		bullet_manager:collide(tree_manager.item_groups.bullets, tree_manager.item_groups.enemies, Obj_list_blocks, dt)
+	local _bullets = tree_manager.item_groups.bullets
+	-- tree_manager.item_groups.bullets =
+
 	tree_manager.item_groups.enemies[1].position = sum_table(tree_manager.item_groups.enemies[1].position, _player_move)
 	tree_manager.item_groups.enemies[1].collider[1] = tree_manager.item_groups.enemies[1].position[1]
 	tree_manager.item_groups.enemies[1].collider[2] = tree_manager.item_groups.enemies[1].position[2]
@@ -69,8 +56,12 @@ function love.update(dt)
 		+ tree_manager.item_groups.enemies[1].s[1]
 	tree_manager.item_groups.enemies[1].collider[4] = tree_manager.item_groups.enemies[1].position[2]
 		+ tree_manager.item_groups.enemies[1].s[2]
-	tree_manager.item_groups.bullets = bullet_manager:move_bullets(tree_manager.bullets, dt)
+
+	tree_manager.item_groups.bullets =
+		bullet_manager:collide(_bullets, tree_manager.item_groups.enemies, Obj_list_blocks, dt) -- Collide ??
+	tree_manager.item_groups.bullets = bullet_manager:move_bullets(tree_manager.item_groups.bullets, dt)
 	tree_manager.item_groups = tree_manager:clean()
+	psystem:update(dt)
 end
 
 function love.draw()
@@ -82,8 +73,8 @@ function love.draw()
 	)
 	if #tree_manager.item_groups.bullets ~= 0 then
 		for _i, _obj in ipairs(tree_manager.item_groups.bullets) do
-			print(_i)
 			love.graphics.circle("fill", _obj.position[1], _obj.position[2], 4)
+			love.graphics.draw(psystem, _obj.position[1], _obj.position[2])
 		end
 	end
 	-- enemies_manager:draw()
@@ -99,6 +90,6 @@ function love.draw()
 			.. "Draw calls: "
 			.. love.graphics.getStats().drawcalls
 			.. " "
-			.. love.graphics.getStats().drawcallsbatched,
+			.. love.graphics.getStats().texturememory,
 	}, 20, 20)
 end
--- objects/bullet_manager.lua
+++ objects/bullet_manager.lua
@@ -24,48 +24,59 @@ function bullet_manager:new(_sprite, _pos, _vel, _dmg, _cust, _cust_movement, _c
 end
 
 function bullet_manager:bullet_death(_bullet, _obj, _dt)
-	local _xd = _obj and _obj:damage(_bullet.damage)
+	if _obj then
+		_obj:damage(_bullet.damage)
+	end
 	_bullet.die(_dt)
 	_bullet = "clean"
+	return _bullet
 end
 
 function bullet_manager:collide(_bullets_pack, _enemies_pack, _towers_pack, _dt)
 	-- Call all the collide functions, also call destroy, die, damage and whatever is in custom_collider_code
 	if (not _bullets_pack) or (#_bullets_pack == 0) then
-		return {}
+		return {} -- IF there is nothing, I want SOMETHING to exists. This could do a makeover for some problems. I don't care
 	end
 	-- THIS IS NOT OPTIMIZED; WTF. PLS USE YOUR BRAIN. #######
 	local _new_bullet_pack = {}
-	for _i, _bullet in ipairs(_bullets_pack) do
-		if _bullet.is_custom then
-			_bullet.custom_collider(_bullets_pack, _enemies_pack, _towers_pack, _dt)
-		else
-			local _collide, _here, _obj = collision_raycast(_bullet.position, _bullet.velocity, _enemies_pack)
-			if _collide then
-				_bullet = bullet_manager:bullet_death(_bullet, _obj, _dt)
+	for _i, _bullet in pairs(_bullets_pack) do
+		if _bullet ~= "clean" then -- Do not touch "clean" state. Those will be removed shortly.
+			if _bullet.is_custom then -- Custom things go by their custom methods.
+				_bullet.custom_collider(_bullets_pack, _enemies_pack, _towers_pack, _dt)
+			else
+				local _collide, _here, _obj = collision_raycast(_bullet.position, _bullet.velocity, _enemies_pack)
+				-- I like how functions work in Lua ngl
+				if _collide then
+					_bullet = bullet_manager:bullet_death(_bullet, _obj, _dt)
+				end
 			end
 		end
 		table.insert(_new_bullet_pack, _bullet)
 	end
-	return _new_bullet_pack
+	return _new_bullet_pack -- YES IT WORKS OH MY GOD
 end
 
 function bullet_manager:move_bullets(_bullets_pack, _dt)
 	if (not _bullets_pack) or #_bullets_pack == 0 then
-		return {}
+		return {} -- The same as before
 	end
-	local _new_bullet_pacl = {}
-	for _i, _bullet in ipairs(_bullets_pack) do
-		if _bullet.is_custom then
-			_bullet:custmon_movement()
-		else
-			_bullet.position = sum_table(_bullet.position, _bullet.velocity)
+	local _new_bullet_pack = {}
+	for _i, _bullet in pairs(_bullets_pack) do
+		if _bullet ~= "clean" then
+			if _bullet.is_custom then
+				_bullet:custmon_movement() -- custom things
+			else
+				_bullet.position = sum_table(_bullet.position, _bullet.velocity) -- Yes, it just sums the velocity. Most of them are just constant velocities, why bother doing more?
+			end
+			if _bullet.position[1] > (love.graphics.getPixelWidth() + 100) or _bullet.position[1] < -100 then
+				_bullet = bullet_manager:bullet_death(_bullet, false, _dt)
+			else
+				if _bullet.position[2] > (love.graphics.getPixelHeight() + 100) or _bullet.position[2] < -100 then
+					_bullet = bullet_manager:bullet_death(_bullet, false, _dt)
+				end
+			end -- Both borders check. One at a time for better performance (I don't know if that works... but it fell it)
 		end
-		if _bullet.position[1] > (love.graphics.getPixelWidth() + 100) or _bullet.position[1] < -100 then
-			_bullet = bullet_manager:bullet_death(_bullet, _obj, _dt)
-		end
-		if _bullet.position[2] > (love.graphics.getPixelHeight() + 100) or _bullet.position[2] < -100 then
-			_bullet = bullet_manager:bullet_death(_bullet, _obj, _dt)
-		end
+		table.insert(_new_bullet_pack, _bullet) -- wiwiwiwiwiwii
 	end
+	return _new_bullet_pack -- wawawa .3 (:3 - ·)
 end
